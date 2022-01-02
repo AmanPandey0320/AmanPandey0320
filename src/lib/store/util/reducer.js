56918d72fb96075692995f3789b1fbfc5b10185b
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2022-01-02 10:03:07 +0530

    cleanu[

--- src/lib/store/util/reducer.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- * @param {*} state 
- * @param {*} action 
- */
-export const setKeyValue = (state, action) => {
-  const { key, value } = action.payload;
-  state.data[key] = value;
-};
--- src/lib/utils/util.js
+++
@@ -1,7 +0,0 @@
-export const getRole = () => {
-  let role = window.localStorage.getItem("role");
-  if (role === undefined || role === null) {
-    role = "portfolio";
-  }
-  return role;
-};
