a8d4a0086b7d39582acbb3c27b1309c4c8dfa710
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2025-03-27 23:21:39 +0530

    added experience data

--- src/assets/data/experienceData.ts
+++ src/assets/data/experienceData.ts
@@ -3,6 +3,8 @@ import java from "@/assets/icons/java.svg";
 import docker from "@/assets/icons/docker.svg";
 import kubernetes from "@/assets/icons/kubernetes.svg";
 import springBoot from "@/assets/icons/spring-boot.svg";
+import oracleImg from "@/assets/images/oracle.png";
+import { BusinessCenterOutlined } from "@mui/icons-material";
 
 export default {
     "header": "Professional Experience",
@@ -13,6 +15,9 @@ export default {
             title:"Associate Software Developer",
             company:"Oracle India Pvt. Ltd.",
             duration:"Aug 2023 - Present",
+            image: oracleImg,
+            summary:"Working on cutting-edge cloud native applications and leading a team of talented engineers to deliver high-quality software solutions for 5G-Core network.",
+            dotIcon: BusinessCenterOutlined,
             details:[
                 "Spearheaded and executed comprehensive benchmarking strategies to evaluate the performance of the product, identifying key areas for optimization and ensuring alignment with industry standards and performance targets.",
                 "Enabled multiple admin access in IAM, enhancing role-based management and control over user permissions. Implemented comprehensive logging of critical user events, ensuring improved tracking and auditability of activities. Developed and configured key metrics pegging, enabling efficient monitoring and analysis of user interactions and system usage",
