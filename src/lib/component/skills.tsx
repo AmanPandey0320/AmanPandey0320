fc6d496b8779d65b11bc52241888f92588724053
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2025-08-10 12:12:06 +0530

    dark mode of skills

--- src/lib/component/skills.tsx
+++ src/lib/component/skills.tsx
@@ -1,9 +1,9 @@
 import skillsData from "@/assets/data/skillsData";
-import { MdCheckCircle, MdDevicesOther } from "react-icons/md";
+import { MdCheckCircle } from "react-icons/md";
 
 export default function Skills() {
     return (
-        <section id="skills" className="min-h-screen p-16 pt-24 flex flex-col gap-2 bg-gray-200" >
+        <section id="skills" className="min-h-screen p-16 pt-24 flex flex-col gap-2 bg-gray-200 dark:bg-gray-900" >
             <h1 className="text-center text-4xl font-bold text-gray-800 dark:text-gray-200">
                 {"Knowledge base"}
             </h1>
@@ -11,22 +11,22 @@ export default function Skills() {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 pt-8">
                 {
                     skillsData.skills.map((sk, i) => {
-                        return (<div key={`${sk.id}-${i}`} className="flex flex-col gap-4 bg-gray-50 p-4 rounded shadow">
+                        return (<div key={`${sk.id}-${i}`} className="flex flex-col gap-4 bg-gray-50 dark:bg-gray-800 p-4 rounded shadow">
                             <div className="flex flex-row gap-2 items-center text-xl">
-                                <span className="text-blue-600 bg-blue-600/10 p-3 rounded-full">
+                                <span className="text-blue-600 dark:text-gray-200 bg-blue-600/10 dark:bg-gray-100/10 p-3 rounded-full">
                                     <sk.icon/>
                                 </span>
-                                <span className="font-semibold">
+                                <span className="font-semibold text-gray-900 dark:text-gray-200">
                                     {sk.title}
                                 </span>
                             </div>
                             <div className="flex flex-col gap-2 px-3">
                                 {
                                     sk.detail.map((dt,j) => (<div key={`${sk.id}_${i}_${j}`} className="flex flex-row gap-2 items-center">
-                                        <span className="text-lg text-green-600">
+                                        <span className="text-lg text-green-600 dark:text-gray-300">
                                             <MdCheckCircle />
                                         </span>
-                                        <span className="text-md">
+                                        <span className="text-md text-gray-600 dark:text-gray-400">
                                             {dt}
                                         </span>
                                     </div>))
